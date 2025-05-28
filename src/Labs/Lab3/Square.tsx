dad1ba2fa217efa876c9c90673e601fe555b5079
ushinn49 <cao.yuchen2@northeastern.edu>
2025-05-28 08:35:41 +0800

    fix bugs

--- src/Labs/Lab3/Square.tsx
+++ src/Labs/Lab3/Square.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export default function Square({ children }: { children: ReactNode }) {
   const num = Number(children);
