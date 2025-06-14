cc3afc5448b3ba8982502d9930b2cff08e13846c
Yuchen Cao <146129989+ushinn49@users.noreply.github.com>
2025-06-14 11:33:45 +0800

    Update client.ts

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER || "http://localhost:4000";
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 const axiosWithCredentials = axios.create({ withCredentials: true });
 
