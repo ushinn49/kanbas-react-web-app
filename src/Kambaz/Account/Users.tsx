8105a5a10208520190b258e0d5f2a72a10d46ad2
Yuchen Cao <146129989+ushinn49@users.noreply.github.com>
2025-06-26 17:01:15 +0800

    Update Users.tsx

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -26,15 +26,15 @@ export default function Users() {
         }
     };
 
-    const filterUsersByName = async (name: string) => {
-        setName(name);
-        if (name) {
-            const users = await client.findUsersByPartialName(name);
-            setUsers(users);
+      const filterUsersByName = async (typedName: string) => {
+        setName(typedName);
+        if (typedName) {
+          const users = await client.findUsersByPartialName(typedName);
+          setUsers(users);
         } else {
-            fetchUsers();
+          fetchUsers();
         }
-    };
+      };
 
     const createUser = async () => {
         const user = await client.createUser({
